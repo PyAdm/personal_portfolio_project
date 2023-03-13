8d1d13baf397e1fee1d752ca4eeeeb7e80b12332
PyAdm <dimald0812@gmail.com>
2023-03-13 20:56:09 +0600

    Third commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Posts
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    posts = Posts.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'posts':posts})
+
