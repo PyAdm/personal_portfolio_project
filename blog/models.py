63ec974e1e00c693b24f10dacc8e86b58163043a
PyAdm <dimald0812@gmail.com>
2023-03-23 16:37:02 +0600

    48.Polirol

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Posts(models.Model):
     title = models.CharField(max_length=100) 
     date = models.DateTimeField(auto_now=True)
-    description = models.CharField(max_length=250)
+    description = models.TextField()
     
+    def __str__(self):
+        return self.title 
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Posts
 
 def all_blogs(request):
-    posts = Posts.objects.order_by('-date')[:5]
+    posts = Posts.objects.order_by('-date')
     return render(request, 'blog/all_blogs.html', {'posts':posts})
 
 def detail(request, post_id):
