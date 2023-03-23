2b8965221d8cfc5515dcec10280848aef8717b6e
PyAdm <dimald0812@gmail.com>
2023-03-23 13:09:38 +0600

    detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Posts
 
 def all_blogs(request):
     posts = Posts.objects.order_by('-date')[:5]
     return render(request, 'blog/all_blogs.html', {'posts':posts})
 
+def detail(request, post_id):
+    post = get_object_or_404(Posts, pk=post_id)
+    return render(request, 'blog/detail.html', {'post':post})
